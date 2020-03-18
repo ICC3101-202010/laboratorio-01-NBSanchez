33bc5fca35e74b37999f0b74bf32144c6d2444d7
NBSanchez <Nbsanchez@miuandes.cl>
2020-03-18 17:20:56 -0300

    Primero parte a

---
+++ Laboratorio 1-SanchezNatalia/Laboratorio 1-SanchezNatalia/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1_SanchezNatalia
+{
+    public class Persona
+    {
+        private string Nombre;
+        private string Apellido;
+
+        public void Lanzar()
+        {
+            Random random = new Random();
+            Console.WriteLine(random.Next(0, 3));
+
+        }
+
+    }
+}
---
+++ Laboratorio 1-SanchezNatalia/Laboratorio 1-SanchezNatalia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1_SanchezNatalia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Lanzar();
+
+            
+
+
+
+
+        }
+    }
+}
